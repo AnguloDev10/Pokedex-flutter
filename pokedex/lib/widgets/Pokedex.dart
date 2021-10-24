509edc99d02b656a971629c4809a3f6c63a6e59b
William <wmavalle@gmail.com>
2021-10-24 02:43:35 -0500

    feat: gesture detector

--- pokedex/lib/widgets/Pokedex.dart
+++ pokedex/lib/widgets/Pokedex.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/utils/colors.dart';
 
 class Pokedex extends StatefulWidget {
   final Widget child;
@@ -12,6 +13,62 @@ class _PokedexState extends State<Pokedex> {
   bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
-    
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 1.7,
+              decoration: BoxDecoration(
+                  color: lighterBlue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                  image: DecorationImage(
+                      image: AssetImage('assets/pokedex_cima.png'),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            height: isExpanded
+                ? MediaQuery.of(context).size.height -
+                    (2 * MediaQuery.of(context).size.width / 1.7)
+                : 0,
+            color: lighterBlue,
+            child: widget.child,
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / 1.7,
+                decoration: BoxDecoration(
+                    color: lighterBlue,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    image: DecorationImage(
+                        image: AssetImage('assets/pokedex_cima.png'),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
