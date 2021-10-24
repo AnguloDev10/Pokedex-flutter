0d5d4a083c06a381c3ef67ecd86c7abe781ad3f0
William <wmavalle@gmail.com>
2021-10-24 02:38:40 -0500

    feat: pokedex

---
+++ pokedex/lib/widgets/Pokedex.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Pokedex extends StatefulWidget {
+  final Widget child;
+  Pokedex({this.child});
+
+  @override
+  _PokedexState createState() => _PokedexState();
+}
+
+class _PokedexState extends State<Pokedex> {
+  bool isExpanded = false;
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
