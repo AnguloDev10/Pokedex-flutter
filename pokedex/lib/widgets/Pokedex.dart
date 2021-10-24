6fe9bc51390e33eb1308ebcd303d5d96546b9124
William <wmavalle@gmail.com>
2021-10-24 12:29:57 -0500

    feat: pokedex background

--- pokedex/lib/widgets/Pokedex.dart
+++ pokedex/lib/widgets/Pokedex.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex/utils/colors.dart';
 
 class Pokedex extends StatefulWidget {
+
   final Widget child;
   Pokedex({this.child});
 
@@ -11,12 +12,14 @@ class Pokedex extends StatefulWidget {
 
 class _PokedexState extends State<Pokedex> {
   bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+
           GestureDetector(
             onTap: () {
               setState(() {
@@ -25,26 +28,25 @@ class _PokedexState extends State<Pokedex> {
             },
             child: Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 1.7,
+              height: MediaQuery.of(context).size.width/1.7,
               decoration: BoxDecoration(
-                  color: lighterBlue,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20)),
-                  image: DecorationImage(
-                      image: AssetImage('assets/pokedex_cima.png'),
-                      fit: BoxFit.cover)),
+                color: lighterBlue,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                image: DecorationImage(
+                  image: AssetImage('assets/pokedex_cima.png'),
+                  fit: BoxFit.cover
+                )
+              ),
             ),
           ),
+
           AnimatedContainer(
             duration: Duration(seconds: 1),
-            height: isExpanded
-                ? MediaQuery.of(context).size.height -
-                    (2 * MediaQuery.of(context).size.width / 1.7)
-                : 0,
+            height: isExpanded ? MediaQuery.of(context).size.height-(2*MediaQuery.of(context).size.width/1.7) : 0,
             color: lighterBlue,
             child: widget.child,
           ),
+
           GestureDetector(
             onTap: () {
               setState(() {
@@ -55,18 +57,19 @@ class _PokedexState extends State<Pokedex> {
               quarterTurns: 2,
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width / 1.7,
+                height: MediaQuery.of(context).size.width/1.7,
                 decoration: BoxDecoration(
-                    color: lighterBlue,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)),
-                    image: DecorationImage(
-                        image: AssetImage('assets/pokedex_cima.png'),
-                        fit: BoxFit.cover)),
+                  color: lighterBlue,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                  image: DecorationImage(
+                    image: AssetImage('assets/pokedex_cima.png'),
+                    fit: BoxFit.cover
+                  )
+                ),
               ),
             ),
           ),
+
         ],
       ),
     );
---
+++ pokedex/lib/widgets/camera.dart
@@ -0,0 +1,171 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+typedef void Callback(List<dynamic> list);
+
+class Camera extends StatefulWidget {
+  final Callback setRecognitions;
+  final double size;
+  Camera(this.setRecognitions, this.size, {Key key}) : super(key: key);
+
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  CameraController cameraController;
+  Future<void> _initializeControllerFuture;
+  bool isDetecting = false;
+  List cameras;
+  int selectedCameraIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCameras();
+  }
+
+  void _initCameras() {
+    availableCameras().then((value) {
+      cameras = value;
+      if (cameras.length > 0) {
+        setState(() {
+          selectedCameraIndex = 0;
+        });
+        initCamera(cameras[selectedCameraIndex]).then((value) {});
+      } else {
+        print('No camera available');
+      }
+    }).catchError((e) {
+      print('Error : ${e.code}');
+    });
+  }
+
+  // SECTION InitCamera
+  Future initCamera(CameraDescription cameraDescription) async {
+    // Si ya existe un controlador entonces soltar los recursos de la camara
+    if (cameraController != null) {
+      await cameraController.dispose();
+    }
+    // Crear un nuevo controllador para la camara
+    // Este objeto permite interactuar con las librerias nativas en dispositivos moviles
+    cameraController =
+        CameraController(cameraDescription, ResolutionPreset.medium);
+
+    // Inicializar el controlador y configurar la camara
+    _initializeControllerFuture = cameraController.initialize().then((_) {
+      // Configuracion de la camara
+      cameraController.setFocusMode(FocusMode.locked);
+      cameraController.setFocusPoint(Offset(0.5, 0.5));
+      // Evitar el iniciar el streaming más de una vez
+      if (cameraController.value.isStreamingImages) return;
+      // Iniciar el streaming de imagenes desde la camara y predecir sobre ella
+      cameraController.startImageStream((CameraImage img) {
+        if (isDetecting) return;
+        int startTime = new DateTime.now().millisecondsSinceEpoch;
+        isDetecting = true;
+        // NOTE Ejecutar modelo y obtener predicción
+        Tflite.runModelOnFrame(
+          bytesList: img.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: img.height,
+          imageWidth: img.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          numResults: 4,
+          threshold: 0.6,
+        ).then((recognitions) {
+          int endTime = new DateTime.now().millisecondsSinceEpoch;
+          print("Detection took ${endTime - startTime}");
+          // Una vez obtenida una predicción detener el streaming
+          if (cameraController.value.isStreamingImages) {
+            cameraController.stopImageStream();
+          }
+          // NOTE Ejecutal "Callback"
+          widget.setRecognitions(recognitions);
+          isDetecting = false;
+        }).timeout(Duration(seconds: 5), onTimeout: _onTimeout);
+      });
+    });
+  }
+  // !SECTION InitCamera
+
+  // SECTION Detection
+  void _imagePrediction(CameraImage img) {
+    // Retornar si existe una detección en proceso.
+    if (isDetecting) return;
+    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    isDetecting = true;
+    // NOTE Ejecutar modelo y obtener predicción
+    Tflite.runModelOnFrame(
+      bytesList: img.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      imageHeight: img.height,
+      imageWidth: img.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 4,
+      threshold: 0.4,
+    ).then((recognitions) {
+      int endTime = new DateTime.now().millisecondsSinceEpoch;
+      print("Detection took ${endTime - startTime}");
+      // Una vez obtenida una predicción detener el streaming
+      if (cameraController.value.isStreamingImages) {
+        cameraController.stopImageStream();
+      }
+      // NOTE Ejecutal "Callback"
+      widget.setRecognitions(recognitions);
+      isDetecting = false;
+    }).timeout(Duration(seconds: 5), onTimeout: _onTimeout);
+    // });
+  }
+
+  void _onTimeout() {
+    // Una vez obtenida una predicción detener el streaming
+    if (cameraController.value.isStreamingImages) {
+      cameraController.stopImageStream();
+    }
+    dynamic data = {};
+    data["index"] = 0;
+    widget.setRecognitions(data);
+    isDetecting = false;
+  }
+
+  // !SECTION Detection
+  // SECTION Mostrar la Camara
+  Widget cameraPreview() {
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // NOTE Una vez se cumpla el "future" mostar la camara
+          return FittedBox(
+            fit: BoxFit.fill,
+            child: Container(
+              width: widget.size,
+              height: cameraController.value.aspectRatio,
+              child: CameraPreview(cameraController),
+            ),
+          );
+        } else {
+          // NOTE De lo contrario mostrar un "loading"
+          return Center(
+            child: Container(
+              width: 50,
+              height: 50,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return cameraPreview();
+  }
+  // !SECTION Mostrar la Camara
+}
