293fd669233b8340e0ccb863adf7449afdac2a09
William <wmavalle@gmail.com>
2021-10-25 12:02:03 -0500

    feat: pokemon data

---
+++ pokedex/lib/widgets/Label.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Labels extends StatelessWidget {
+  final List<dynamic> _recognitions;
+
+  Labels(this._recognitions);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget _renderLabels() {
+      return Column(
+        children: _recognitions != null
+            ? _recognitions.map((res) {
+                return Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Text(
+                    "${res["index"]} - ${res["label"]}: ${res["confidence"].toStringAsFixed(3)}",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      background: Paint()..color = Colors.white,
+                    ),
+                  ),
+                );
+              }).toList()
+            : [],
+      );
+    }
+
+    return Center(
+      child: _renderLabels(),
+    );
+  }
+}
