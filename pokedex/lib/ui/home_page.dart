cd9ce187c45fe6ebd3ae171e0534164fab6e8847
William <wmavalle@gmail.com>
2021-11-01 21:27:13 -0500

    finished: all my tasks was done

--- pokedex/lib/ui/home_page.dart
+++ pokedex/lib/ui/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/widgets/Pokedex.dart';
+import 'package:pokedex/widgets/PokemonSecondaryCard.dart';
 import 'package:pokedex/widgets/pokemon_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,7 +20,13 @@ class _HomePageState extends State<HomePage> {
               child: 
                 Pokemon_Card(),
             ),
-            
+            Expanded(child: Row(children: [
+              SizedBox(width: 133.0),
+              
+              PokemonSecondaryCard('OeYw991.png'),
+              SizedBox(width: 20.0),
+              PokemonSecondaryCard('WDr1JrI.png')
+            ],))
           ],
         ),
       ),
