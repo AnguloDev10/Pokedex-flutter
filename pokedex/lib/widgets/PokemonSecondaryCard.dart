4208085b492aeab6c6e67ce0fe91d34bd091d8be
Setsuke <sergio_alva99@hotmail.com>
2021-11-02 15:07:35 -0500

    ready

--- pokedex/lib/widgets/PokemonSecondaryCard.dart
+++ pokedex/lib/widgets/PokemonSecondaryCard.dart
@@ -1,53 +1,56 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/main.dart';
+import 'package:pokedex/ui/detection_page.dart';
 import 'package:pokedex/ui/flutter_dex.dart';
 import 'package:pokedex/utils/colors.dart';
 
 class PokemonSecondaryCard extends StatelessWidget {
   final String link;
   PokemonSecondaryCard(this.link);
-@override
+  @override
   Widget build(BuildContext context) {
-  return GestureDetector(
-     onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => FlareFlutterDex(cameras),
-              ),
-            );
-          },
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => FlareFlutterDex(cameras),
+          ),
+        );
+      },
       onDoubleTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => FlareFlutterDex(cameras),
-              ),
-            );
-          },
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen(),
+          ),
+        );
+      },
       child: AspectRatio(
         aspectRatio: 1,
         child: Container(
           padding: EdgeInsets.all(5),
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [lightBlue0, lightBlue]
-            ),
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [lightBlue0, lightBlue]),
             borderRadius: BorderRadius.circular(10),
           ),
           child: Column(
             children: [
               Expanded(
                 flex: 3,
-                child: Image.network('https://i.imgur.com/'+link, fit: BoxFit.cover,),
-                )
+                child: Image.network(
+                  'https://i.imgur.com/' + link,
+                  fit: BoxFit.cover,
+                ),
+              )
             ],
           ),
         ),
       ),
-  );
+    );
   }
 }
